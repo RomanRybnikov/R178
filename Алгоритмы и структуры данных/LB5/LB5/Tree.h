7e91d325b81701dd5d11cfef31a147effe84bd09
DESKTOP-LLQ0HAO\Konstantin <kkohno@mail.ru>
2020-11-16 02:26:02 +0700

    лаба 5

---
+++ Алгоритмы и структуры данных/LB5/LB5/Tree.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <iostream>
+
+template<typename T>
+class Tree
+{
+	class Node {
+		T m_Value;
+		Node* m_Left;
+		Node* m_Right;
+
+		void AddLeft(T value) {
+			if (m_Left) {
+				m_Left->Add(value);
+				return;
+			}
+			m_Left = new Node(value);
+		}
+		void AddRight(T value) {
+			if (m_Right) {
+				m_Right->Add(value);
+				return;
+			}
+			m_Right = new Node(value);
+		}
+	public: 
+		Node(T value) {
+			m_Value = value;
+			m_Left = 0;
+			m_Right = 0;
+		}
+
+		void Add(T value) {
+			if (value < m_Value) AddLeft(value);
+			else AddRight(value);
+		}
+
+		std::ostream& Print(std::ostream& os) {
+			os << '(' << m_Value << ' ';
+			if (m_Left) m_Left->Print(os);
+			else os << "()";
+			if (m_Right) m_Right->Print(os);
+			else os << "()";
+			os << ')';
+			return os;
+		}
+	};
+
+	Node* m_Root;
+
+	void Parse(std::istream& is)
+	{
+		// очистка
+		delete m_Root;
+
+		// читаем значение
+		T value;
+		is >> value;
+
+		// если фейл то ничего нет
+		if (is.fail()) return;
+
+		// создаем коренной узел
+		m_Root = new Node(value);
+
+		// вставка в корень пока чтото читается
+		while (true) {
+			// читаем значение
+			is >> value;
+			if (is.fail()) return;
+			// вставка
+			m_Root->Add(value);
+		}
+	}
+
+public:
+	Tree(std::istream& is) {
+		m_Root = 0;
+		Parse(is);
+	}
+
+	template<typename T1>
+	friend std::ostream& operator << (std::ostream & os, Tree<T1> &t);
+};
+
+template<typename T1>
+std::ostream& operator << (std::ostream & os, Tree<T1> &t) {
+	if (!t.m_Root) return os << "()";	
+	return t.m_Root->Print(os);
+}
+
---
+++ Алгоритмы и структуры данных/LB5/LB5/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <locale.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Tree.h"
+
+using namespace std;
+
+int main(){
+	setlocale(0, "RUS");
+	stringstream is("4 2 1 3 6 5 7");
+
+	//Tree<int> t(cin); 
+	Tree<int> t(is);
+
+	cout << "Построенное дерево: " << t << endl;
+
+	system("PAUSE");
+	return 0;
+}
